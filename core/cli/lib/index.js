16eaf7e543b4e795e192dda36e820e7a5bca38d6
charlessliu <charlessliu@tencent.com>
2023-06-12 23:13:47 +0800

    feat: 检查node版本

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,16 +2,33 @@
 
 module.exports = core;
 
+const semver = require('semver')
+const colors = require('colors')
+
 const pkg = require('../package.json')
 const log = require('@fake-cli-dev/log')
-
+const constant = require('./constants')
 
 function core() {
-  console.log('Hello from core')
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  } catch (e) {
+    log.error(e.message)
+  }
 }
 
 // 检查版本号
 function checkPkgVersion() {
   log.info('cli', pkg.version)
+}
+
+// 检查node版本号
+function checkNodeVersion() {
+  // 获取当前node版本号，并比对最低版本号
+  const currentVersion = process.version
+  const lowestNodeVersion = constant.LOWEST_NODE_VERSION
+  if (semver.lte(currentVersion, lowestNodeVersion)) {
+    throw new Error(colors.red(`fake-cli 需要安装 v${lowestNodeVersion} 以上版本的 Node.js`))
+  }
 }
